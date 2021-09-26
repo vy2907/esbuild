4db2f788c077e6559e56590796fbb274bd52b90c
vy2907 <vynguyen29071997@gmail.com>
2021-09-26 22:26:25 +0700

    60

---
+++ jbook/src/index.tsx
@@ -0,0 +1,39 @@
+import * as esbuild from "esbuild-wasm";
+import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
